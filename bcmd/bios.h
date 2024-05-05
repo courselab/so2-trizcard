e6585be09bc465a94b6918c55fdc1a15a9fcb577
Heitor Tanoue <heitortanoue@usp.br>
2024-05-05 12:08:56 -0300

    Add new files and functions (bcmd)

---
+++ bcmd/bios.h
@@ -0,0 +1,20 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
+ */
+
+#ifndef BIOS_H
+#define BIOS_H
+
+void __attribute__((fastcall)) clear(void);
+void __attribute__((fastcall)) print();
+void __attribute__((fastcall)) println();
+void __attribute__((fastcall)) readln(char *);
+
+// new function that will be called from main.c
+void __attribute__((fastcall)) get_date(unsigned int*, unsigned char*, unsigned char*);
+
+#endif
