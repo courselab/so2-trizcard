5a79f989ac6fb3cec406a84832acb8eb83d84b5e
trizcard <beatrizco@usp.br>
2024-05-06 10:59:28 -0300

    .fill and .word func

--- Assignment 1/hwasm.c
+++ Assignment 1/hwasm.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 
 #define MAX_INSTRUCTION_LENGTH 256
+#define MAX_LABELS 128
 
 // Function to replace the file extension
 void replace_extension(char *filename, const char *new_extension)
@@ -26,8 +27,41 @@ void write_bytes(FILE *fp, const unsigned char *bytes, size_t num_bytes)
      fwrite(bytes, 1, num_bytes, fp);
 }
 
+typedef struct
+{
+     char label[256];
+     int address;
+} Label;
+
+Label labelTable[MAX_LABELS];
+int labelCount = 0;
+
+void addLabel(const char *label, int address)
+{
+     if (labelCount < MAX_LABELS)
+     {
+          strcpy(labelTable[labelCount].label, label);
+          labelTable[labelCount].address = address;
+          labelCount++;
+     }
+}
+
+Label findLabelAddress(const char *label)
+{
+     for (int i = 0; i < labelCount; i++)
+     {
+          if (strstr(labelTable[i].label, label) != NULL)
+          {
+               return labelTable[i];
+          }
+     }
+     Label notFound;
+     notFound.address = -1;
+     return notFound; // Label not found
+}
+
 // Encode and write MOV, INT, HLT, and JMP instructions
-void encode_and_write_instruction(FILE *fp, const char *instruction, const char *operand)
+void encode_and_write_instruction(FILE *fp, const char *instruction, const char *operand, int currentAddress)
 {
      if (strcmp(instruction, "movb") == 0 && operand[3] == 'e')
      {
@@ -51,17 +85,22 @@ void encode_and_write_instruction(FILE *fp, const char *instruction, const char 
      }
      else if (strcmp(instruction, "jmp") == 0)
      {
-          // Simple interpretation for short backward jump to hlt (assuming hlt is the previous instruction)
-          unsigned char opcode[] = {0xEB, (unsigned char)strtol(operand, NULL, 0)}; // jmp short (relative address)
+          Label labelResp = findLabelAddress(operand);
+          printf("Label: %s, Address: %d\n", labelResp.label, labelResp.address);
+          if (labelResp.address == -1)
+          {
+               fprintf(stderr, "Label '%s' not found\n", operand);
+               return;
+          }
+          int offset = labelResp.address - currentAddress + strlen(labelResp.label) + 1; // for the size of the jmp instruction itself
+          unsigned char opcode[] = {0xEB, (unsigned char)(offset & 0xFF)};
           write_bytes(fp, opcode, sizeof(opcode));
      }
+     else
+     {
+          addLabel(instruction, currentAddress);
+     }
 }
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define MAX_INSTRUCTION_LENGTH 256
 
 // Handle .fill directive
 void handle_fill(FILE *fp, int count, int size, int value)
@@ -80,24 +119,39 @@ void handle_word(FILE *fp, int word)
      write_bytes(fp, bytes, 2);
 }
 
-// Parse directive and handle it
-void parse_and_handle_directive(FILE *fp, char *line)
+void parseAndHandleDirective(FILE *fp, const char *directive, const char *params)
 {
-     int param1, param2, param3;
-     if (sscanf(line, ".fill %d, %d, %d", &param1, &param2, &param3) == 3)
+     if (strcmp(directive, ".word") == 0)
      {
-          handle_fill(fp, param1, param2, param3);
+          unsigned char opcode[] = {0xB0, (unsigned char)strtol(params + 1, NULL, 0)};
+          write_bytes(fp, opcode, sizeof(opcode));
+          printf("Word: %s\n", opcode);
      }
-     else if (sscanf(line, ".word 0x%x", &param1) == 1)
+     else if (strcmp(directive, ".fill") == 0)
      {
-          handle_word(fp, param1);
+          int repeat, size, value;
+          sscanf(params, "%d, %d, %d", &repeat, &size, &value);
+          for (int i = 0; i < repeat; i++)
+          {
+               for (int j = 0; j < size; j++)
+               {
+                    write_bytes(fp, (const unsigned char *)&value, sizeof(char));
+               }
+          }
+          printf("Fill: %d, %d, %d\n", repeat, size, value);
+     }
+     else
+     {
+          fprintf(stderr, "Unknown directive: %s\n", directive);
      }
 }
 
 // Function to trim leading spaces
-const char* trimLeadingSpaces(const char *str) {
-    while(isspace((unsigned char)*str)) str++;
-    return str;
+const char *trimLeadingSpaces(const char *str)
+{
+     while (isspace((unsigned char)*str))
+          str++;
+     return str;
 }
 
 int main(int argc, char *argv[])
@@ -131,27 +185,36 @@ int main(int argc, char *argv[])
           return EXIT_FAILURE;
      }
 
+     int currentAddress = 0; // Inicialização de currentAddress
      char line[MAX_INSTRUCTION_LENGTH];
      while (fgets(line, MAX_INSTRUCTION_LENGTH, source) != NULL)
      {
-          char *token = strtok(line, " ,\t\n");
-          char instruction[MAX_INSTRUCTION_LENGTH];
-          char operand[10] = {0};
+          char *token = strtok(line, " \t\n");
           const char *trimmed = trimLeadingSpaces(line);
-          if (trimmed[0] == '.')
-          { // Check if it's a directive
-               parse_and_handle_directive(fp, line);
-          }
-          else if (token != NULL)
+          if (token && trimmed[0] == '.')
           {
+               printf("Directive: %s\n", token);
+               char *directive = token;
+               char operand[10] = {0};
+               if (token != NULL)
+               {
+                    strcpy(operand, token); // Assuming operand is next token
+               }
+               parseAndHandleDirective(fp, directive, operand);
+          }
+          else if (token)
+          {
+               char instruction[MAX_INSTRUCTION_LENGTH];
+               char operand[10] = {0};
                strcpy(instruction, token);
                token = strtok(NULL, " ,\t\n");
                if (token != NULL)
                {
                     strcpy(operand, token); // Assuming operand is next token
                }
-               encode_and_write_instruction(fp, instruction, operand);
+               encode_and_write_instruction(fp, instruction, operand, currentAddress);
           }
+          currentAddress += strlen(line);
      }
 
      fclose(source);
